e9b5bc2ceecaef731cc6ffad6cf8bab8aaad0d53
programmergoutam <155313517+programmergoutam@users.noreply.github.com>
2024-01-28 12:08:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("a").on("click", function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
+            window.location.hash = hash;
+          }
+        );
+      }
+    });
+  });
+  
+  $(".menu-items a").click(function () {
+    $("#checkbox").prop("checked", false);
+  });
+  
